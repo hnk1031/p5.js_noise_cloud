731a5d7d815bb14f8e097138d52089b07fa4356f
Honoka Atsuchi <>
2019-07-25 15:33:03 +0900

    雲を表現

---
+++ sketch.js
@@ -0,0 +1,59 @@
+const tileCount = 100;
+const noiseScale = 0.05;
+
+var grid;
+var xnoise;
+var ynoise;
+var t = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(150, 180, 255);
+  createGrid();
+  showGrid();
+  t += 0.01;
+}
+
+function createGrid() {
+  grid = [];
+  let tileSize = width / tileCount;
+  ynoise = t;
+  for (let row = 0; row < tileCount; row++) {
+    grid[row] = [];
+    xnoise = t;
+    for (let col = 0; col < tileCount; col++) {
+      let x = col * tileSize;
+      let y = row * tileSize;
+      let a = noise(xnoise, ynoise) * 255;
+      grid[row][col] = new Tile(x, y, tileSize, a);
+      xnoise += noiseScale;
+    }
+    ynoise += noiseScale;
+  }
+}
+
+function showGrid() {
+  for (let row = 0; row < tileCount; row++) {
+    for (let col = 0; col < tileCount; col++) {
+      grid[row][col].show();
+    }
+  }
+}
+
+class Tile {
+  constructor(x, y, size, a) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.c = color(255, a);
+  }
+
+  show() {
+    noStroke();
+    fill(this.c);
+    rect(this.x, this.y, this.size, this.size);
+  }
+}
